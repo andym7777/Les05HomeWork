a9ccd79c95cf66d0b0ae8b80328f5933a149226d
andym7777 <37099541+andym7777@users.noreply.github.com>
2018-04-01 10:04:02 +0300

    Update fraction.java

--- src/andym/fraction.java
+++ src/andym/fraction.java
@@ -1,6 +1,17 @@
 package andym;
 
 public class fraction {
-	int k = 0;
-	String fracArray = "";
+	private int n;
+	private int m;
+
+	public fraction(int numerator, int denomominator) {
+		this.n = numerator;
+		this.m = denomominator;
+	}
+
+	public String toString() {
+
+		return  n + "/" + m + ";";
+
+	}
 }
